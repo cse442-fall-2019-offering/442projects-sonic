c97f719bd9f9cd966f25250c68c21984b9b499ec
Shawn <shawnmam@buffalo.edu>
2019-10-15 13:17:38 -0400

    live search is working and added extra text to items in list

--- screens/Nutrition.js
+++ screens/Nutrition.js
@@ -1,5 +1,4 @@
-
-import React, {Component} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,128 +14,109 @@ import {
   Input,
   TouchableHighlight
 } from 'react-native';
-import { ListItem, SearchBar } from 'react-native-elements';
-
+import { SearchBar, ListItem, Icon } from 'react-native-elements';
 
 
 
 var API = 'https://trackapi.nutritionix.com/v2/search/instant?query=';
-var DEFAULT_QUERY = 'ap';
+var DEFAULT_QUERY = '';
 
 class Nutrition extends React.Component  {
- constructor(props) {
+  constructor(props){
     super(props);
-
-    this.state = {
-      loading: false,
-      data: [],
-      error: null,
-    };
-
-    this.arrayholder = [];
+    this.state = { isLoading: true }
   }
 
-  componentDidMount() {
-    this.makeRemoteRequest();
-  }
-
-  makeRemoteRequest = () => {
-
-    this.setState({ loading: true });
-
-    return fetch(API + DEFAULT_QUERY,
-
-        {
-          headers:{
-            'x-app-id' : '979e48c8',
-            'x-app-key': 'e7cc162c38e1ee157bcad82667783fef'
-          }
-        })
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({
-          data: responseJson.branded,
-          loading: false,
-        },function(){
-
-        });
-        this.arrayholder = responseJson.branded;
+  fetchData(text) {
+    this.setState({ text });
+    const url = 'https://trackapi.nutritionix.com/v2/search/instant?query=';
+    fetch(url + text,
+      {
+        headers:{
+          'x-app-id' : '979e48c8',
+          'x-app-key': 'e7cc162c38e1ee157bcad82667783fef'
+        }
       })
-      .catch(error => {
-        this.setState({ error, loading: false });
+      .then(response => response.json())
+      .then((responseJson) => {
+        this.setState({
+          dataSource: responseJson.branded,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-  };
-
-  renderSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: '86%',
-          backgroundColor: '#CED0CE',
-          marginLeft: '14%',
-        }}
-      />
-    );
-  };
-
-  searchFilterFunction = text => {
-    this.setState({
-      value: text,
-    });
+  }
 
 
 
-    const newData = this.arrayholder.filter(item => {
-      const itemData = `${item.food_name.toUpperCase()}`;
-      const textData = text.toUpperCase();
 
-      return itemData.indexOf(textData) > -1;
-    });
-    this.setState({
-      data: newData,
-    });
-  };
+   render(){
 
-  renderHeader = () => {
-    return (
-      <SearchBar
-        placeholder="Type Here..."
-        lightTheme
-        round
-        onChangeText={text => this.searchFilterFunction(text)}
-        autoCorrect={false}
-        value={this.state.value}
-      />
-    );
-  };
+      return(
 
-  render() {
-    if (this.state.loading) {
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <ActivityIndicator />
+
+        <View style={styles.container}>
+
+
+             <TextInput
+                 placeholder="Type your food..."
+                 underlineColorAndroid = "#000000"
+                 onChangeText={(text) => { this.fetchData(text); }}
+               //  inlineImageLeft='fastfood-24px.svg'
+
+             />
+
+             <FlatList
+                 data={this.state.dataSource}
+                 renderItem={({ item }) => (
+
+                 <ListItem
+                     leftAvatar={{ source: { uri:  `${item.image} `} }}
+                     title={`${item.food_name} `}
+                     subtitle={'Calories: '+`${item.nf_calories}`}
+                  />
+
+                  )}
+
+                  keyExtractor={({id}, index) => id}
+
+             />
+
         </View>
+
+
       );
     }
-    return (
-      <View style={{ flex: 1 }}>
-        <FlatList
-          data={this.state.data}
-          renderItem={({ item }) => (
-            <ListItem
-             // leftAvatar={{ source: { uri:  } }}
-              title={`${item.food_name} `}
-             // subtitle={}
-            />
-          )}
-          keyExtractor={({id}, index) => id}
-          ItemSeparatorComponent={this.renderSeparator}
-          ListHeaderComponent={this.renderHeader}
-        />
-      </View>
-    );
   }
-}export default Nutrition;
 
 
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 24,
+      backgroundColor: 'white',
+    },
+    content: {
+      paddingBottom: 300,
+    },
+    card1: {
+      paddingVertical: 16,
+    },
+    card2: {
+      padding: 16,
+    },
+    input: {
+      marginTop: 4,
+    },
+    title: {
+      paddingBottom: 16,
+      textAlign: 'center',
+      color: '#404d5b',
+      fontSize: 20,
+      fontWeight: 'bold',
+      opacity: 0.8,
+    },
+  });
+  export default Nutrition;
