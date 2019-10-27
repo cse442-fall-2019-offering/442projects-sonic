afa941a7bb8ff9306b08ad08956da7e50de253a5
lwu8891 <lwu3@buffalo.edu>
2019-10-27 00:49:10 -0400

    Changed button message

--- screens/Login.js
+++ screens/Login.js
@@ -95,7 +95,7 @@ class Login extends React.Component {
             <Text>If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
 
 The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Website URL, unless otherwise defined in this Privacy Policy. </Text>
-            <Button title="Hide modal" onPress={this.toggleModal} />
+            <Button title="Close" onPress={this.toggleModal} />
           </View>
         </Modal>
  
