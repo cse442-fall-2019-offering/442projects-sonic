0178a1f52ae1e56647059ff01bdb7a98d6402efc
sedrehman <syedrehm@buffalo.edu>
2019-11-06 10:34:08 -0500

    database added and log in and create account working

--- screens/Login.js
+++ screens/Login.js
@@ -1,22 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
-    View,
-    Text,
-    Button,
-    Image,
-    TouchableHighlight,
-    StyleSheet
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableHighlight,
+  StyleSheet,
+  Alert
 } from 'react-native';
 import { Input } from 'react-native-elements';
+import * as firebase from 'firebase';
 
 
 
 
 class Login extends React.Component {
 
+
+  state = {
+    user: null,
+    isLoggedIn: false,
+    good: false
+  }
+
+  componentDidMount() {
+
+    // To Configure react native app with cloud of Google Firebase database !
+    var config = {
+      apiKey: "AIzaSyAhQDAzgnHPymQkc1BNeB0sPKJTvPfZ20c",
+      authDomain: "nutrition-go.firebaseapp.com",
+      databaseURL: "https://nutrition-go.firebaseio.com",
+      projectId: "nutrition-go",
+      storageBucket: "nutrition-go.appspot.com",
+      messagingSenderId: "353598024085",
+      appId: "1:353598024085:web:b3e7f5645a5705a4186e9c",
+      measurementId: "G-CLRT6JSHLW"
+    };
+    firebase.initializeApp(config);
+
+  }
+
+  user_login(email_, password_) {
+    var good = firebase.auth().signInWithEmailAndPassword(email_, password_).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      alert('here');
+
+    });
+    this.state.isLoggedIn = true;
+  }
+
+
   render() {
 
-    const {navigate} = this.props.navigation;
+    const { navigate } = this.props.navigation;
 
     return (
 
@@ -34,21 +78,30 @@ class Login extends React.Component {
         <Input
           placeholder='Username'
           placeholderTextColor='#ffff66'
+          onChangeText={text => this.setState({ email: text })}
         />
 
         <Input
           placeholder='Password'
           placeholderTextColor='#ffff66'
+          onChangeText={text => this.setState({ password: text })}
+
         />
 
         <View style={{ padding: 15, marginTop: 10 }} >
 
           <TouchableHighlight
             style={styles.loginButtonStyle}
-            onPress={() => Alert.alert('Log In button pressed')}
+            onPress={() => {
+              //Alert.alert('Log In button pressed');
+              this.user_login(this.state.email, this.state.password)
+              if(this.state.isLoggedIn){
+                navigate('SearchScreen')
+              }
+            }}
           >
 
-            <Text style = {{fontWeight : 'bold'}}> LOG IN </Text>
+            <Text style={{ fontWeight: 'bold' }}> LOG IN </Text>
 
           </TouchableHighlight>
 
@@ -73,20 +126,20 @@ class Login extends React.Component {
 
 const styles = StyleSheet.create({
 
-   loginButtonStyle:{
-        height: 40,
-        width: 150,
-        borderRadius: 3,
-        alignItems: 'center',
-        backgroundColor: '#ffff66',
-        padding: 10
-   },
+  loginButtonStyle: {
+    height: 40,
+    width: 150,
+    borderRadius: 3,
+    alignItems: 'center',
+    backgroundColor: '#ffff66',
+    padding: 10
+  },
 
-   imageStyle: {
-        width: 60,
-        height: 60,
-        justifyContent: "space-evenly"
-   }
+  imageStyle: {
+    width: 60,
+    height: 60,
+    justifyContent: "space-evenly"
+  }
 
 });
 
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,182 +1,318 @@
+
+
 import React from 'react';
-import {
-    View,
-    Text,
-    StyleSheet,
-    DatePickerAndroid,
-    Picker,
-    ScrollView,
-    StatusBar,
-    TextInput,
-    Image
-} from 'react-native';
-import {Button,Icon} from 'react-native-elements'
+import { View, Text, StyleSheet, DatePickerAndroid, Picker, ScrollView, StatusBar, TextInput, Image, Alert } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import LinearGradient from 'react-native-linear-gradient';
+import { Button, Icon } from 'react-native-elements';
+import * as firebase from 'firebase';
+
+class Profile extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            choosenLabel: '',
+            choosenindex: '',
+            email: '',
+            fullName: '',
+            password: '',
+            passwordReEnter: '',
+            age: '',
+            location: '',
+            phoneNumber: '',
+            sex: '',
+            height: '',
+            currentWeight: '',
+            goalWeight: '',
+            activity_level: 1.2,
+            tde: 0
+        };
+        this.good = false;
+    }
+    
+    
+    
+
+    // componentWillMount() {
+
+    //     // To Configure react native app with cloud of Google Firebase database !
+    //     var config = {
+    //         apiKey: "AIzaSyAhQDAzgnHPymQkc1BNeB0sPKJTvPfZ20c",
+    //         authDomain: "nutrition-go.firebaseapp.com",
+    //         databaseURL: "https://nutrition-go.firebaseio.com",
+    //         projectId: "nutrition-go",
+    //         storageBucket: "nutrition-go.appspot.com",
+    //         messagingSenderId: "353598024085",
+    //         appId: "1:353598024085:web:b3e7f5645a5705a4186e9c",
+    //         measurementId: "G-CLRT6JSHLW"
+    //     };
+
+    //     //firebase.initializeApp(config);
+
+    // }
+
+    // setUserData() {
+    //     firebase.database().ref('users/' + firebase.auth().currentUser.uid).set({
+    //         email: this.state.email,
+    //         fullName: this.state.fullName,
+    //         password: this.state.password,
+    //         age: Number(this.state.age),
+    //         location: this.state.location,
+    //         phoneNumber: this.state.phoneNumber,
+    //         sex: this.state.sex,
+    //         height: Number(this.state.height),
+    //         currentWeight: Number(this.state.currentWeight),
+    //         goalWeight: Number(this.state.goalWeight),
+    //         foodArray: [],
+    //         tde: Number(this.state.tde),
+    //     });
+    // }
 
 
+    authinticate(email_, fullName_,  password_, passwordReEnter_ , age_, location_, phoneNumber_, 
+        sex_, height_, currentWeight_, goalWeight_, tde_) {
+        if ((email_.includes('@')) && !(password_ === '') && (passwordReEnter_ === password_)) {
+
+            firebase.auth().createUserWithEmailAndPassword(email_, password_).catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode == 'auth/weak-password') {
+                    alert('The password is too weak.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
+
+            firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                    id = user.uid;
+                    if (id != null) {
+                        this.good = true;
+                        
+                        firebase.database().ref('users/' + firebase.auth().currentUser.uid).set({
+                            email: email_,
+                            fullName: fullName_,
+                            password: password_,
+                            age: Number(age_),
+                            location: location_,
+                            phoneNumber: phoneNumber_,
+                            sex: sex_,
+                            height: Number(height_),
+                            currentWeight: Number(currentWeight_),
+                            goalWeight: Number(goalWeight_),
+                            foodArray: [],
+                            tde: Number(tde_),
+                        });
+                    }else{
+                        alert.alert("null id");
+                    }
+                } else {
+                    user.delete();
+                    alert.alert("not ready");
+                }
+              });
+            
+            
+
+            // Alert.alert('good! --' + email_ + ', ' + password_ + ', ' + passwordReEnter_);
+
+        } else {
+            Alert.alert('bad! --' + email_ + ', ' + password_ + ', ' + passwordReEnter_);
+        }
+    }
 
 
-class SignUp extends React.Component {
- 
-  state = { choosenLabel: '', choosenindex: '' };
+    render() {
 
-  render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <LinearGradient colors={['#61045f', '#20011f']} style={styles.LinearGradientView}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    keyboardDismissMode='on-drag' >
 
-    const {navigate} = this.props.navigation;
+                    <StatusBar backgroundColor="black" barStyle="light-content" />
+                    <Text style={styles.SignUpText}> Sign Up </Text>
 
-    return (
 
-       <LinearGradient colors= {['#61045f','#20011f']} style = {styles.LinearGradientView}>
+                    <TextInput style={styles.InputBox}
+                        placeholder='Email Address'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ email: text })}
+                    />
 
-           <ScrollView
-               showsVerticalScrollIndicator = {false}
-               keyboardDismissMode = 'on-drag' >
+                    <TextInput style={styles.InputBox}
+                        placeholder='Full Name'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ fullName: text })}
+                    />
 
-               <StatusBar backgroundColor="black" barStyle="light-content" />
-               <Text style = {styles.SignUpText}> Sign Up </Text>
+                    <TextInput style={styles.InputBox}
+                        placeholder='Password'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ password: text })}
+                    />
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Email Address'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
+                    <TextInput style={styles.InputBox}
+                        placeholder='Re-enter password'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ passwordReEnter: text })}
+                    />
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Full Name'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
+                    <TextInput style={styles.InputBox}
+                        placeholder='Age'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ age: text })}
+                    />
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Password'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
+                    <TextInput style={styles.InputBox}
+                        placeholder='Location'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ location: text })}
+                    />
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Re-enter password'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
+                    <TextInput style={styles.InputBox}
+                        placeholder='Phone Number'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ phoneNumber: text })}
+                    />
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Date of Birth'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
+                    <View>
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Location'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
-
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Phone Number'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
-
-                   <View>
-
-                        <Text style={{color:'white'}}>Sex</Text>
-
-                        <Picker style = {{color:'white'}}
-                            selectedValue={this.state.choosenLabel}
+                        <Text style={{ color: 'white' }}>Sex</Text>
+                        <Picker style={{ color: 'white' }}
+                            selectedValue={this.state.sex}
                             onValueChange={(itemValue, itemIndex) =>
-                            this.setState({choosenLabel: itemValue, choosenindex: itemIndex })
-                        }>
+                                //this.state.sex = itemValue
+                                this.setState({ sex : itemValue})
+                            }>
 
-                           <Picker.Item label ='Male' value ='Male' />
-                           <Picker.Item label = 'Female' value = 'Female' />
-
+                            <Picker.Item label='Male' value='Male' />
+                            <Picker.Item label='Female' value='Female' />
                         </Picker>
 
-                   </View>
+                    </View>
+
+                    <TextInput style={styles.InputBox}
+                        placeholder='Height (in inches)'
+                        keyboardType='numeric'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ height: text })}
+                    />
+
+                    <TextInput style={styles.InputBox}
+                        placeholder='Current Weight'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ currentWeight: text })}
+                    />
+
+                    <TextInput style={styles.InputBox}
+                        placeholder='Goal Weight'
+                        placeholderTextColor='#ffffff'
+                        underlineColorAndroid='transparent'
+                        onChangeText={text => this.setState({ goalWeight: text })}
+                    />
 
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Height'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
+                    <View style={{ paddingBottom: 25 }}>
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Current Weight'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
+                        <Text style={{ color: 'white' }}>Activity Level</Text>
 
-                   <TextInput style= {styles.InputBox}
-                       placeholder='Goal Weight'
-                       placeholderTextColor='#ffffff'
-                       underlineColorAndroid= 'transparent'
-                   />
-
-
-                   <View>
-
-                        <Text style={{color:'white'}}>Activity Level</Text>
-
-                        <Picker style = {{color:'white'}}
-                            selectedValue={this.state.choosenLabel}
+                        <Picker style={{ color: 'white', paddingBottom: 10 }}
+                            selectedValue={this.state.activity_level}
                             onValueChange={(itemValue, itemIndex) =>
-                            this.setState({ choosenLabel: itemValue, choosenindex: itemIndex })
-                        }>
+                                this.setState({ choosenLabel: itemValue, choosenindex: itemIndex, activity_level: itemValue })
+                            }>
 
-                           <Picker.Item label ='Sedentary (little to no exercise)' value ='1.2' />
-                           <Picker.Item label ='Lightly Active (lightly exercise/sports 1-3 days/week)' value = '1.375' />
-                           <Picker.Item label ='Moderately Active (moderate exercise/sports 3-5 days/week)' value = '1.55' />
-                           <Picker.Item label ='Very Active (hard exercise/sports 6-7 days/week)' value = '1.725' />
-                           <Picker.Item label ='Extremely Active (very heavy exercise/physical job/training twice a day)' value = '1.9'/>
+                            <Picker.Item label='Sedentary (little to no exercise)' value='1.2' />
+                            <Picker.Item label='Lightly Active (lightly exercise/sports 1-3 days/week)' value='1.375' />
+                            <Picker.Item label='Moderately Active (moderate exercise/sports 3-5 days/week)' value='1.55' />
+                            <Picker.Item label='Very Active (hard exercise/sports 6-7 days/week)' value='1.725' />
+                            <Picker.Item label='Extremely Active (very heavy exercise/physical job/training twice a day)' value='1.9' />
                         </Picker>
 
-                   </View>
+                    </View>
 
 
-                   <Button
-                       large
-                       raised
-                       rounded= {true}
-                       title= "Let's Get Started!"
-                       icon = {{name:'ios-cloud-done', type:'ionicon'}}
-                       onPress={() => navigate('SearchScreen')}
-                   />
+                    <Button
+                        large
+                        raised
+                        rounded={true}
+                        title="Let's Get Started!"
+                        icon={{ name: 'ios-cloud-done', type: 'ionicon', buttonStyle: styles.someButtonStyle }}
+                        onPress={() => {
+                            //navigate('Nutrition', { name: 'Jane' })
+                            //this.authinticate(this.state.email, this.state.password, this.state.passwordReEnter)
+                            if (this.state.sex === 'Male') {
+                                this.setState({tde: (66 + (13.7 * (Number(this.state.currentWeight) * 2.2)) + 
+                                    (5 * 2.54 * Number(this.state.height) - (6.8 * Number(this.state.age)))) * Number(this.state.activity_level)})
+                            } else {
+                                this.setState({ted: (655 + (9.6 * (Number(this.state.currentWeight) * 2.2)) + (1.8 * 2.54 * Number(this.state.height) - 
+                                    (4.7 * Number(this.state.age)))) * Number(this.state.activity_level)})
+                            }
 
-           </ScrollView>
+                            this.authinticate(this.state.email, this.state.fullName, this.state.password, 
+                                this.state.passwordReEnter, this.state.age, this.state.location, this.state.phoneNumber, 
+                                this.state.sex, this.state.height, this.state.currentWeight, this.state.goalWeight, this.state.tde)
 
-       </LinearGradient>
 
-    );
+                            if (this.good) {
+                                navigate('Nutrition')
+                            }
 
-  }
+                        }}
+                    />
 
+
+
+
+
+                </ScrollView>
+            </LinearGradient>
+
+
+        );
+    }
 }
 
 
 const styles = StyleSheet.create({
 
-   LinearGradientView:{
-       flex:1,
-       alignItems: 'center',
-   },
+    LinearGradientView: {
+        flex: 1,
+        alignItems: 'center',
+    },
 
-   SignUpText:{
-       fontSize: 44,
-       color: 'white',
-       padding: 50
-   },
+    SignUpText: {
+        fontSize: 44,
+        color: 'white',
+        padding: 50
+    },
 
-   InputBox:{
-       width: 300,
-       backgroundColor: '#0C0032',
-       borderRadius: 25,
-       paddingHorizontal: 16,
-       marginVertical: 10,
-       color: '#ffffff'
+    InputBox: {
+        width: 300,
+        backgroundColor: '#0C0032',
+        borderRadius: 25,
+        paddingHorizontal: 16,
+        marginVertical: 10,
+        color: '#ffffff'
 
-   },
+    },
+
+
 
 });
 
-export default SignUp;
+export default Profile;
